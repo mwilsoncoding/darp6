d73c548e165a4e52f1e722c6000e18471aa55700
Max Wilson <max@maxwilson.dev>
2020-08-07 18:56:26 -0400

    fix io ref

--- flake.nix
+++ flake.nix
@@ -3,15 +3,16 @@
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/2d6cbbe4627f6fe4a179c681537b0a3e4f59b732;
   inputs.system76AcpiDkms.url = github:mwilsoncoding/system76-acpi-dkms/944395eff1e6882a47a57584be08c6ad7fd8cdfd;
-  inputs.system76IoDkms.url = github:mwilsoncoding/system76-acpi-dkms/b00e2c00958dea3977efbae18d0e5476213201ab;
+  inputs.system76IoDkms.url = github:mwilsoncoding/system76-io-dkms/b00e2c00958dea3977efbae18d0e5476213201ab;
 
   outputs = { self, nixpkgs, system76AcpiDkms, system76IoDkms }: {
 
-  nixosConfigurations.darp6 = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-      system76AcpiDkms.nixosModules.system76-acpi-dkms
-      system76IoDkms.nixosModulus.system76-io-dkms
-    ];
+    nixosConfigurations.darp6 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        system76AcpiDkms.nixosModules.system76-acpi-dkms
+        system76IoDkms.nixosModulus.system76-io-dkms
+      ];
+    };
   };
 }
