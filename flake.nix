3442d6c3103a83e7b831911f054a178653f5675b
Max Wilson <max@maxwilson.dev>
2020-08-17 15:38:58 -0400

    rename modules

--- flake.nix
+++ flake.nix
@@ -1,13 +1,21 @@
 {
   description = "Bare configuration for a System76 Darter Pro 6";
 
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/b3251e04ee470c20f81e75d5a6080ba92dc7ed3f;
   inputs.system76AcpiDkms.url = github:mwilsoncoding/system76-acpi-dkms-flake/f5b2a0f55985b5b5b004ae74c030db5f2208c3ca;
   inputs.system76IoDkms.url = github:mwilsoncoding/system76-io-dkms-flake/40ebd9f6a687f481152333d8c709e2f4c6100e72;
 
-  outputs = { self, system76AcpiDkms, system76IoDkms }: {
+  outputs = { self, nixpkgs, system76AcpiDkms, system76IoDkms }: {
 
     nixosModules = {
-      system76-acpi-dkms =
+      compatibleKernel =
+        {system, ...}:
+        {
+          config = {
+            boot.kernelPackages = (import nixpkgs {inherit system;}).linuxPackages_latest;
+          };
+        };
+      system76AcpiDkms =
         {
           config = {
             boot.extraModulePackages = [ system76AcpiDkms.defaultPackage.x86_64-linux ];
@@ -17,7 +25,7 @@
             boot.kernelModules = [ "system76_acpi" ];
           };
         };
-      system76-io-dkms =
+      system76IoDkms =
         {
           config = {
             boot.extraModulePackages = [ system76IoDkms.defaultPackage.x86_64-linux ];
