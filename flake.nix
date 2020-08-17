c45914af2b2f3f391c05cec1624e836579fae883
Max Wilson <max@maxwilson.dev>
2020-08-17 15:42:09 -0400

    import

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Bare configuration for a System76 Darter Pro 6";
 
+  # This nixpkgs must be the same one used to build the system76 packages
   inputs.nixpkgs.url = github:NixOS/nixpkgs/b3251e04ee470c20f81e75d5a6080ba92dc7ed3f;
   inputs.system76AcpiDkms.url = github:mwilsoncoding/system76-acpi-dkms-flake/f5b2a0f55985b5b5b004ae74c030db5f2208c3ca;
   inputs.system76IoDkms.url = github:mwilsoncoding/system76-io-dkms-flake/40ebd9f6a687f481152333d8c709e2f4c6100e72;
